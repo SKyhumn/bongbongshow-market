8cfcc2ba5bed049e92f431c235697a31f825ceb9
ChoiJunYoung <s24052@gsm.hs.kr>
2025-12-16 21:01:44 +0900

    홈 페이지 UI 개발 중

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -4,6 +4,11 @@ export default function Home(){
     return( 
         <div className="home-page">
             <Header/>
+            <div className="content">
+                <div className="box1"></div>
+                <div className="box2"></div>
+                <div className="box3"></div>
+            </div>
         </div>
     );
 }
