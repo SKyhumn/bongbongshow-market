e3df019615e44e05ba33df60b2b39527412cca92
ChoiJunYoung <s24052@gsm.hs.kr>
2025-12-16 21:02:27 +0900

    홈페이지 개발 중

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -5,9 +5,15 @@ export default function Home(){
         <div className="home-page">
             <Header/>
             <div className="content">
-                <div className="box1"></div>
-                <div className="box2"></div>
-                <div className="box3"></div>
+                <div className="box1">
+                    
+                </div>
+                <div className="box2">
+
+                </div>
+                <div className="box3">
+                    
+                </div>
             </div>
         </div>
     );
